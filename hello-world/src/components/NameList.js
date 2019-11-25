f9831088eb31a0ed515e5827acc3c6d3d9534bb4
Yvon N <ynchonon@emoneyadvisor.com>
2019-11-25 15:42:25 -0500

    refactor nameList

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -2,13 +2,8 @@ import React from 'react'
 
 function NameList() {
     const name =['Bruce', 'Clark', 'Diana']
-    return (
-        <div>
-            {
-                name.map(name=><h2>{name}</h2>)
-            }
-        </div>
-    )
+    const nameList= name.map(name =><h2>{name}</h2>)
+    return <div>{nameList}</div>
 }
 
 export default NameList
