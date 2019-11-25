b53cd77cd1c49ead2c3ef68179c12de7fe5f6bc3
Yvon N <ynchonon@emoneyadvisor.com>
2019-11-25 10:46:29 -0500

    Call a method of a parent comp from a button in the child components by pass the method as prop to a child component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,18 +2,21 @@ import React, {Component} from 'react';
 // import logo from './logo.svg';
 // import Greet from './components/Greet' ;
 import './App.css';
+import ParentComponent from './components/ParentComponent';
 // import Welcome from './components/Welcome';
 // import Message from './components/Message';
 // import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+
 
 class App extends Component {
   render(){
     return(
       <div className= 'App'>
-        <EventBind/>
+        <ParentComponent/>
+        {/* <EventBind/> */}
         {/* <ClassClick/>
         <FunctionClick/> */}
         {/* <Counter/> */}
