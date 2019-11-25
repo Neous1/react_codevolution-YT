8b7ba027a0155b5045a741a291b06bfd26e99342
Yvon N <ynchonon@emoneyadvisor.com>
2019-11-25 05:28:29 -0500

    event handler in a function comp

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,27 +1,29 @@
 import React, {Component} from 'react';
 // import logo from './logo.svg';
-import Greet from './components/Greet' ;
+// import Greet from './components/Greet' ;
 import './App.css';
-import Welcome from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
+// import Welcome from './components/Welcome';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
 
 class App extends Component {
   render(){
     return(
       <div className= 'App'>
-        <Counter/>
+        <FunctionClick/>
+        {/* <Counter/> */}
         {/* <Message/> */}
-        <Greet name='Bruce' heroName='Batman'>
+        {/* <Greet name='Bruce' heroName='Batman'>
           <p>This is a children props</p>
         </Greet>
         <Greet name='Clark' heroName='Superman'>
           <button>Action</button>  
         </Greet>
-        <Greet name='Diana' heroName='Wonder Woman'/>
-        <Welcome name='Bruce' heroName='Batman'/>
+        <Greet name='Diana' heroName='Wonder Woman'/> */}
+        {/* <Welcome name='Bruce' heroName='Batman'/>
         <Welcome name='Clark' heroName='Superman'/>
-        <Welcome name='Diana' heroName='Wonder Woman'/> 
+        <Welcome name='Diana' heroName='Wonder Woman'/>  */}
       
 
       </div>
