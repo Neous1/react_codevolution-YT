95c56538d36d7cdfacabdd787d5a44938b4e6781
Yvon N <ynchonon@emoneyadvisor.com>
2019-11-25 05:36:35 -0500

    Class comp event handler

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -6,11 +6,13 @@ import './App.css';
 // import Message from './components/Message';
 // import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 class App extends Component {
   render(){
     return(
       <div className= 'App'>
+        <ClassClick/>
         <FunctionClick/>
         {/* <Counter/> */}
         {/* <Message/> */}
