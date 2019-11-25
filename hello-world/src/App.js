00335600517f601df78db9359fd61b505b1ebae9
Yvon N <ynchonon@emoneyadvisor.com>
2019-11-25 11:13:05 -0500

    Use IfElse for conditional rendering

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 // import Greet from './components/Greet' ;
 import './App.css';
 import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
 // import Welcome from './components/Welcome';
 // import Message from './components/Message';
 // import Counter from './components/Counter';
@@ -15,7 +16,8 @@ class App extends Component {
   render(){
     return(
       <div className= 'App'>
-        <ParentComponent/>
+        <UserGreeting/>
+        {/* <ParentComponent/> */}
         {/* <EventBind/> */}
         {/* <ClassClick/>
         <FunctionClick/> */}
---
+++ hello-world/src/components/UserGreeting.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+    constructor(props){
+        super(props)
+
+        this.state={
+            isLoggedIn: true
+        }
+    }
+    render() {
+        if (this.state.isLoggedIn) {
+            return(
+                <div>Welcome Yvon</div>
+            )
+        } else {
+            return(
+                <div>Welcome Guest</div>
+            )
+        }
+        // return (
+        //     <div>
+        //        <div>Welcome Yvon </div>
+        //        <div>Welcome Guest</div>
+        //     </div>
+        // )
+    }
+}
+
+export default UserGreeting
